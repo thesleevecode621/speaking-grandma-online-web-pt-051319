75b736a7bf56561c0bc06af729abe944089c15b0
boruch d <dubywider@gmail.com>
2019-08-12 15:20:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma(speak)
   
   if speak == "I LOVE YOU GRANDMA!"
-    puts "HUH?! SPEAK UP, SONNY!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   elsif speak == 'Hi Nana, how are you?'
    puts 'HUH?! SPEAK UP, SONNY!'
    elsif speak == "hi"
