6943aaad7958d501d6acfb13336919d5b0a4e656
boruch d <dubywider@gmail.com>
2019-08-12 15:26:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(speak)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif speak == 'Hi Nana, how are you?'
    return "HUH?! SPEAK UP, SONNY!"
-   elsif speak == "hi!"
+   elsif speak == "Hi!"
    return "HUH?! SPEAK UP, SONNY!"
  else 
    return "NO, NOT SINCE 1938!"
